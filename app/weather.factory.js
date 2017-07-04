53576ec57d15f0147b69f0e354290bef2b72fb13
Brittany Lacoste <brittanylacoste@brittanys-air.attlocal.net>
2017-07-04 13:11:41 -0700

    More comments added

--- app/weather.factory.js
+++ app/weather.factory.js
@@ -6,8 +6,8 @@
         .factory('weatherFactory', weatherFactory);
 
     weatherFactory.$inject = ['$http', '$q'];
-
-    function weatherFactory($http, $q) { // Factory to retrieve weather
+// Factory to retrieve weather
+    function weatherFactory($http, $q) {
         var service = {
             searchForWeather : searchForWeather
         };
@@ -15,9 +15,10 @@
 
         return service;
 
+// Toastr implementation
         function searchForWeather(term) {
           return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + term + '&units=imperial&apikey=c7b29f3fcd3f63017813f8a0b16b2581')
-          } then(function(response) { // Toastr implementation
+          } then(function(response) { 
             defer.resolve(response);
           }, function (error) {
             defer.reject (error);
